e7a19b227697de1a38e1dac4316db2aa04402a87
garradefogo124 <micaelmiranda124@gmail.com>
2022-10-25 17:39:27 -0300

    Versão 2 - adição de obstáculo e conserto de bugs

--- js/main.js
+++ js/main.js
@@ -5,6 +5,8 @@ window.onload = function () {
     showSize = document.querySelector('#show-size')
     showBestSize = document.querySelector('#show-best-size')
 
+    gameOverMessage = document.querySelector('#snackbar')
+
     document.addEventListener("keydown", keyPush)
 
     setInterval(game, 90)
@@ -13,6 +15,8 @@ window.onload = function () {
     let speedX = 0, speedY = 0, pointX = 10, pointY = 10
     let blockSize = 20, blocksAmount = 20
     let appleX = 16, appleY = 7
+    let rockX = Math.floor(Math.random() * blocksAmount)
+    let rockY = Math.floor(Math.random() * blocksAmount)
 
     let trail = []
     let snakeBody = 5
@@ -39,8 +43,12 @@ window.onload = function () {
         context.fillStyle = "#0A952B"
         context.fillRect(0, 0, stage.width, stage.height)
 
+        // Pinta a Maçã
         context.fillStyle = "#ff0000"
         context.fillRect(appleX * blockSize, appleY * blockSize, blockSize, blockSize)
+
+        context.fillStyle = "#667"
+        context.fillRect(rockX * blockSize, rockY * blockSize, blockSize, blockSize)
 
         // Faz a cobrinha
         context.fillStyle = "#e68436"
@@ -48,10 +56,12 @@ window.onload = function () {
         for (let i = 0; i < trail.length; i++) {
             context.fillRect(trail[i].x * blockSize, trail[i].y * blockSize, blockSize - 1, blockSize - 1)
             // Verifica se bateu, para finalizar o jogo
-            if (trail[i].x == pointX && trail[i].y == pointY) {
+            if (trail[i].x == pointX && trail[i].y == pointY
+                && (speedX > 0 || speedY > 0)) {
                 speedX = speedY = 0;
                 snakeBody = 5
                 showSize.textContent = snakeBody
+                showGameOverMessage()
             }
         }
 
@@ -70,33 +80,50 @@ window.onload = function () {
             appleY = Math.floor(Math.random() * blocksAmount)
         }
 
+        // Se bater na rocha, perde o jogo e reinicia
+        if (rockX == pointX && rockY == pointY) {
+            speedX = speedY = 0;
+            snakeBody = 5
+            showSize.textContent = snakeBody
+            rockX = Math.floor(Math.random() * blocksAmount)
+            rockY = Math.floor(Math.random() * blocksAmount)
+            showGameOverMessage()
+        }
 
+        // Define um novo recorde caso o salvo seja menor que a pontuação atual
         if (showBestSize.textContent < snakeBody) {
             showBestSize.textContent = snakeBody
+        }
+
+        // Mostra a mensagem de fim de jogo
+        function showGameOverMessage() {
+            gameOverMessage.className = "show" // Adiciona a classe show
+
+            setTimeout(function () { gameOverMessage.className = gameOverMessage.className.replace("show", ""); }, 3000); // remove a classe show
         }
     }
 
     // Comandos do teclado, cada setinha faz ela se movimentar para um lado
     function keyPush(event) {
-        switch (event.keyCode) {
-            case 37: //left
-                speedX = -speed
-                speedY = 0
-                break
-            case 38: // up
-                speedX = 0
-                speedY = -speed
-                break
-            case 39: //right
-                speedX = speed
-                speedY = 0
-                break
-            case 40: // down
-                speedX = 0
-                speedY = speed
-                break
-            default:
-                break
+        if (event.keyCode == 37 && speedX == 0) { // esquerda
+            speedX = -speed
+            speedY = 0
+            return
+        }
+        if (event.keyCode == 38 && speedY == 0) { // cima
+            speedX = 0
+            speedY = -speed
+            return
+        }
+        if (event.keyCode == 39 && speedX == 0) { // direita
+            speedX = speed
+            speedY = 0
+            return
+        }
+        if (event.keyCode == 40 && speedY == 0) { // baixo
+            speedX = 0
+            speedY = speed
+            return
         }
     }
 
